9768a27412467e4c62068e315b9bf103246d42c2
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-11-13 08:26:02 +0100

    fish fzf uses fd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 set fish_greeting ""
+alias fd=fdfind
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin /snap/bin $HOME/go/bin $PATH
+set -x FZF_DEFAULT_COMMAND 'fd --type f'
 set -x GOPATH $HOME/go
 bass source /etc/profile
