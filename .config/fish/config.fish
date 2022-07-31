046cdc677b11661f31ded46fad24ce04c04426f2
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2022-07-31 20:28:31 +0200

    wanip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set fish_greeting ""
 alias fd=fdfind
+alias wanip='dig @resolver4.opendns.com myip.opendns.com +short'
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin /snap/bin $HOME/go/bin $PATH
 set -x FZF_DEFAULT_COMMAND 'fd --type f'
 set -x GOPATH $HOME/go
