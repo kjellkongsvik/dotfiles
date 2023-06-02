d3064c4dbfd4c83b41ca0046e5f3f8c4ad782ef7
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2023-06-02 15:30:35 +0200

    helix updates +++

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,4 @@ set -x PATH $HOME/.local/bin $HOME/.cargo/bin /snap/bin $HOME/go/bin $PATH
 set -x FZF_DEFAULT_COMMAND 'fd --type f'
 set -x GOPATH $HOME/go
 # bass source /etc/profile
+pyenv init - | source
