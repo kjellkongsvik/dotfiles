3a562e9d70445cad39b3178572c28e808a5a70d5
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2024-06-06 21:45:17 +0200

    simpler fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,2 @@
 set fish_greeting ""
-alias fd=fdfind
-alias wanip='dig @resolver4.opendns.com myip.opendns.com +short'
-set -x PATH $HOME/.local/bin $HOME/.cargo/bin /snap/bin $HOME/go/bin $HOME/.dotnet/tools $PATH
-set -x FZF_DEFAULT_COMMAND 'fd --type f'
-set -x GOPATH $HOME/go
-set -x DOTNET_ROOT /usr/share/dotnet
-# bass source /etc/profile
-pyenv init - | source
+set -x PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH
