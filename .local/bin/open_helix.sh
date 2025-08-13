6a12d7f52e8b50d2461f28b266e69a80eee52150
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2025-08-13 20:21:22 +0200

    revert back to tmux-fingers
    Improve `open-helix.sh` script

--- .local/bin/open_helix.sh
+++ .local/bin/open_helix.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-fileName=`wl-paste`
-absFileName=$(readlink -f $fileName)
-pane_id=$(tmux list-panes -s -F "#{pane_id} #{pane_current_command}" | awk '$2 == "hx" { print $1; exit }')
+file_name=`wl-paste`
+abs_file_name=$(readlink -f $file_name)
+window_index=$(tmux display-message -p '#{window_index}')
+pane_id=$(tmux list-panes -t $window_index -F "#{pane_id} #{pane_current_command}" | awk '$2 == "hx" { print $1; exit }')
 tmux select-pane -t $pane_id
 tmux send-keys -t $pane_id Escape
-tmux send-keys -t $pane_id ":o $absFileName" Enter
+tmux send-keys -t $pane_id ":o "$abs_file_name
+tmux send-keys -t $pane_id Enter
