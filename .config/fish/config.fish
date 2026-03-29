636d3aee7e2c212d0b8e9e70a08bac98c91360fd
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2026-03-29 23:38:17 +0200

    thumbs: refactor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ set fish_greeting ""
 set -x EDITOR $HOME/.cargo/bin/hx
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH
 if status is-interactive
-    bind \ch thumbs_helix
-    bind \ck "zellij ac dump-screen | thumbs -u | wl-copy"
+    bind \ch "thu hx;"
+    bind \ck "thu;"
 end
