0434ae4faa018b2de23edf80664a79259b8f1db3
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2026-03-29 23:38:17 +0200

    thumbs: copy and open in helix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
 set fish_greeting ""
 set -x EDITOR $HOME/.cargo/bin/hx
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH
-bind \co "ohn;"
+if status is-interactive
+    bind \ch thumbs_helix
+    bind \ck "zellij ac dump-screen | thumbs -u | wl-copy"
+end
