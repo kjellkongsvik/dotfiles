c797d6bb794c0cd5f4ba5ba26fdbc00c23b5868a
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2026-03-29 23:38:17 +0200

    thu: capture thumbs stdout directly, remove temp file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,8 @@
 set fish_greeting ""
 set -x EDITOR $HOME/.cargo/bin/hx
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH
-bind \co "ohn;"
+if status is-interactive
+    eval (zellij setup --generate-auto-start fish | string collect)
+    bind \ch "thu hx;"
+    bind \ck "thu copy;"
+end
