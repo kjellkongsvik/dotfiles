11314bf2d84cdc853a23a48475a8cfc6f324e89d
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2026-09-19 17:28:59 +0200

    zellij thumbs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
 set fish_greeting ""
 set -x EDITOR $HOME/.cargo/bin/hx
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH
-if status is-interactive
-    bind \ch "thu hx;"
-    bind \ck "thu;"
-end
